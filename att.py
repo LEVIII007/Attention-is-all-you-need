44442076602f0850136f09000c71c73fd73746b0
“Shashank <shashanktyagi12345@gmail.com>
2024-04-26 04:32:54 +0530

    implemented encoder and decoder blocks, just integration and decoder main block left

--- att.py
+++ att.py
@@ -61,5 +61,91 @@ class TransformerBlock(nn.Module):
 
 
 class Encoder(nn.Module):
+    def __init__(self,
+                src_vocab_size, 
+                embed_size, 
+                num_layers, 
+                heads, 
+                device, 
+                forward_expansion, 
+                dropout, 
+                max_length):
+        
+        super(Encoder, self).__init___()
+        self.embed_size = embed_size
+        self.heads = heads
+        self.dropout = dropout
+        self.device = device
+        self.word_embedding = nn.Embedding(src_vocab_size, embed_size)
+        self.position_embedding = nn.Embedding(max_length, embed_size)
+
+
+        self.layers = nn.ModuleLlist(
+            [
+            TransformerBlock(
+                embed_size, 
+                heads,
+                dropout = dropout,
+                forward_expansion = forward_expansion,
+            )
+            ]
+        )
+        self.dropout = nn.Dropout(dropout)
+    
+
+    def forward(self, x, mask):
+        # N = num of examples
+        N, seq_length = x.shape
+        positions = torch.arange(0, seq_length).expand(N, seq_length).to(self.device)
+
+        out = self.dropout(self.word_embedding(x)+self.position_embedding(positions))
+        
+        for layer in self.layers:
+            #we are in eocoder, value, key, query are gonna be same : out
+            out = layer(out, out, out, mask)
+
+        return out
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, embed_size,heads,forward_expansion, dropout, device):
+        super(DecoderBlock, self).__init__()
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm = nn.Layers(embed_size)
+        self.transformer_block = TransformerBlock(embed_size, heads, dropout, forward_expansion)
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, x, value, key, src_mask, trg_mask):
+        attention = self.attention(x,x,x,trg_mask)
+        query = self.dropout(self.norm(attention + x))
+        out = self.transformer_block(value, key, query, src_mask)
+        return out
+    
+
+# X is input, x,x,x goes to attention, then norm, then to transformer which is same part in both encoder and encoder.
+# it takes input Q,V,K and output after passing it from attention layer, norm layer
+
+
+
+class Decoder(nn.Module):
+    def __init__(self, 
+                 trg_vocab_size,
+                 num_layers,
+                 embed_size, 
+                 heads,
+                 forward_expansion,
+                 dropout,
+                 device,
+                 max_length):
+        super(Decoder, self).__init__()
+        self.device = device
+        self.word_embedding = nn.embedding(trg_vocab_size, embed_size)
+        self.position_embedding = nn.embedding
+
+
+
+
+
+
 
 
